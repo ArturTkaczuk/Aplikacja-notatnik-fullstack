c448762d3a0581714d111bfef60531b83c68e5e6
Artur Tkaczuk <tkaczukartur@gmail.com>
2023-02-28 15:15:33 +0100

    Create local crud operations (without db)

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const notesRouter = require("./routes/notes");
+const bodyParser = require("body-parser");
 
-app.use(notesRouter);
+// parsers
+app.use(bodyParser.json());
 
+// routes
+app.use("/api/v1/", notesRouter);
+
+// homepage
 app.get("/", (req, res) => {
   res.send("Homepage");
 });
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,26 +1,66 @@
 const express = require("express");
 const router = express.Router();
 
-// Create
+const notes = [
+  {
+    id: 12334,
+    title: "Wash car",
+    description: "And on the way home buy groceries",
+  },
+  {
+    id: 43432,
+    title: "Visit aunt",
+    description: "And dont forget to buy her chocolate",
+  },
+];
+
+// Create note
 router.post("/notes", (req, res) => {
-  res.send("Create note");
+  const title = req.body.title;
+  const description = req.body.description;
+  const id = Math.floor(Math.random() * (10000 - 1) + 1);
+
+  notes.push({ id, title, description });
+
+  res.status(200).json({ id, title, description });
 });
 
-// Read
+// Read notes
 router.get("/notes", (req, res) => {
-  res.send("Get all notes");
+  res.status(200).json(notes);
 });
 
-// Update
+// Update note
 router.put("/notes/:id", (req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
   const id = req.params.id;
-  res.send("Update note with id: " + id);
+
+  const objIndex = notes.findIndex((obj) => obj.id == id);
+
+  if (objIndex == -1) {
+    res.status(422).json({ error: "Wrong note id" });
+  } else {
+    notes[objIndex].title = title;
+    notes[objIndex].description = description;
+
+    res.status(200).json(notes[objIndex]);
+  }
 });
 
-// Delete
+// Delete note
 router.delete("/notes/:id", (req, res) => {
   const id = req.params.id;
-  res.send("Delete note with id: " + id);
+
+  const objIndex = notes.findIndex((obj) => obj.id == id);
+
+  if (objIndex == -1) {
+    res.status(422).json({ error: "Wrong note id" });
+  } else {
+    notes.splice(objIndex, 1);
+
+    res.status(200).send("Note deleted");
+  }
 });
 
 module.exports = router;
