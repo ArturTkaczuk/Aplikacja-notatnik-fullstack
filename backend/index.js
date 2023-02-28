41361222a4ae7a712a8d7ba7c0d1cfa3347c1d57
Artur Tkaczuk <tkaczukartur@gmail.com>
2023-02-28 16:02:13 +0100

    Add initial database config

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,20 @@ const express = require("express");
 const app = express();
 const notesRouter = require("./routes/notesRouter");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 require("dotenv").config();
+
+// database
+async function database() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Success connecting to database");
+  } catch (err) {
+    console.log("Error connecting to the database: " + err);
+  }
+}
+
+database();
 
 // parsers
 app.use(bodyParser.json());
